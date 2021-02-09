0e0d8e7460daa5b2a60f4b026dc8b21c525246c6
Benjamin Kindle <Benjamin.Kindle@hyland.com>
2021-02-09 10:27:48 -0500

    nx g c testcomp --project=testlib

---
+++ libs/testlib/src/lib/testcomp/testcomp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'testco-testcomp',
+  templateUrl: './testcomp.component.html',
+  styleUrls: ['./testcomp.component.scss']
+})
+export class TestcompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/testlib/src/lib/testlib.module.ts
+++ libs/testlib/src/lib/testlib.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TestcompComponent } from './testcomp/testcomp.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [TestcompComponent],
 })
 export class TestlibModule {}
